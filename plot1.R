d234e952e021156a5b21500cddc53c709d86a0f3
bosky2001 <42346504+bosky2001@users.noreply.github.com>
2020-06-22 00:29:14 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,5 +4,5 @@ data<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
 #making the histogram
 windows()
 png(filename = "Plot1.png", width = 480, height = 480, units = "px")
-hist(as.numeric(data[,3]),xlab="Global Active Power(kilowatts)",col="red")
-de
+hist(as.numeric(data[,3]),xlab="Global Active Power(kilowatts)",col="red")#[,3] is the column we use, we can also use data$Global_active_power
+dev.ofF()
