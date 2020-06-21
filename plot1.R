6dde87d7eadd0c928fdc38529bbfd39871a32fed
bosky2001 <42346504+bosky2001@users.noreply.github.com>
2020-06-22 00:23:04 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+#loading data
+data<-read.delim("household_power_consumption.txt",sep=";")
+data<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
+#making the histogram
+windows()
+png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+hist(as.numeric(data[,3]),xlab="Global Active Power(kilowatts)",col="red")
+de
